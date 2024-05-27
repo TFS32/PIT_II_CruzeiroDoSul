2e642d475059bcd9ba6a30acf4c158aaf998c915
Tiago Saraiva <tiagofsaraiva@hotmail.com>
2024-05-27 16:59:34 -0500

    arquivos de media e documents

--- bigquery_access.sql
+++ bigquery_access.sql
@@ -1,1 +1,15 @@
-SELECT * FROM 'league'
+-- Connect to a BigQuery database 
+
+-- SELECT primeiras 10 linhas da tabela match
+SELECT * FROM pitii-sdb.eusdatabase.match LIMIT 10;
+
+SELECT * FROM pitii-sdb.eusdatabase.player LIMIT 50;
+
+SELECT * FROM pitii-sdb.eusdatabase.match_event LIMIT 10;
+
+CREATE TABLE IF NOT EXISTS pitii-sdb.eusdatabase.test (
+    t_id        INT           
+  , example     DECIMAL      
+);
+
+DROP TABLE IF EXISTS pitii-sdb.eusdatabase.test;
